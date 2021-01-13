e3bf0097e7debb866154724ce966414cb5e182a8
Wiktor <wiktor.muraszko@wp.pl>
2021-01-13 23:22:27 +0100

    fix and nn, dikstra beg

--- Algorithms.py
+++ Algorithms.py
@@ -1,5 +1,6 @@
 import random
 import queue
+import numpy as np
 from Graph import Tree, calc_cost_form_leaf, get_path, calc_cost_form_leaf_without_returning
 import logging
 import copy
@@ -10,8 +11,9 @@ class Algorithm:
 
     def __init__(self, graph, symmetrical=True):
         self.graph = graph
+        self.start_node = random.choice(self.graph.all_nodes)
         self.symmetrical = symmetrical
-        self.tree = Tree(random.choice(self.graph.all_nodes), self.graph)
+        self.tree = Tree(self.start_node, self.graph)
 
     def BFS(self):
         costs = []
@@ -48,6 +50,54 @@ class Algorithm:
                 que.append(neighbour)
             first = False
         return costs
+
+    def NN(self):
+        que = [copy.deepcopy(self.tree.root)]
+        first = True
+        first_dist = False
+        while que:
+            node = que.pop()
+            logging.info(f'Current path {get_path(node)}')
+            neighbours = self.tree.create_connected_leafs(node)
+            if node.name == self.tree.root.name and not first:
+                return [calc_cost_form_leaf_without_returning(node, symmetrical=self.symmetrical)]
+            first = False
+            costs = {}
+            for neighbour in neighbours:
+                costs[neighbour.name] = (calc_cost_form_leaf_without_returning(neighbour, symmetrical=self.symmetrical), neighbour)
+            my_min = dict(sorted(costs.items(), key=lambda item: item[1]))
+            for key in my_min:
+                que.append(my_min[key][1])
+
+    def Dijkstra(self):
+        que = [copy.deepcopy(self.tree.root)]
+        costs = {}
+        not_visited = list(self.graph.get_all_nodes_names())
+        for node in not_visited:
+            costs[node] = None, np.inf
+        costs[self.tree.root.name] = copy.deepcopy(self.tree.root), 0
+        while not_visited:
+            node = que.pop()
+            neighbours = self.tree.create_connected_leafs(node)
+            not_visited.remove(node.name)
+            for neighbour in neighbours:
+                tmp_cost = (calc_cost_form_leaf_without_returning(neighbour, symmetrical=self.symmetrical))
+                _, curr_cost = costs[neighbour.name]
+                if tmp_cost < curr_cost:
+                    costs[neighbour.name] = (neighbour, tmp_cost)
+            sub_costs = {key: costs[key] for key in not_visited}
+            for key in sub_costs:
+                _leaf, _cost = sub_costs[key]
+                _min = np.inf
+                if _cost < _min:
+                    _min = _cost
+                    min_leaf = _leaf
+            que.append(min_leaf)
+        return costs
+
+
+def create_tree_from_dict(dictionary):
+    return None
 
 
 
